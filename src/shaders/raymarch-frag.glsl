647abc5126f54fd52455bdacdaf783d854bb497b
viviviantung <90019550+viviviantung@users.noreply.github.com>
2022-10-03 12:31:21 -0400

    colors and shit

--- src/shaders/raymarch-frag.glsl
+++ src/shaders/raymarch-frag.glsl
@@ -5,14 +5,14 @@ precision highp float;
 uniform vec2 u_Dimensions;
 // uniform float u_Fov;
 uniform float u_Time;
-// uniform vec3 u_EyePos;
+uniform vec3 u_Eye;
 // uniform mat4 u_ViewInv; 
 
 out vec4 out_Col;
 
 const int MAX_MARCHING_STEPS = 255;
 const float MIN_DIST = 0.0;
-const float MAX_DIST = 100.0;
+const float MAX_DIST = 50.0;
 const float EPSILON = 0.0001;
 
 // noise ------------------------------------------
@@ -60,15 +60,15 @@ float polyfbm(vec2 p)
     mat2 r3 = rotate(1.0);
     
     // 1st octave
-    float m1 = polynoise(p * r2, .2);
+    float m1 = polynoise(p * r2, .7);
     
-    m1 += polynoise ( r1 * (vec2(0.5, 0.5) + p), .2);
-    m1 += polynoise ( r3 * (vec2(0.35, 0.415) + p), .2);
+    m1 += polynoise ( r1 * (vec2(0.5, 0.5) + p), clamp(sin(u_Time * 0.05), -0.2, 0.9));
+    m1 += polynoise ( r3 * (vec2(0.35, 0.415) + p), clamp(sin(u_Time * 0.1), 0.2, 0.9));
     m1 *= 0.333 * 0.75;
     
     // 2nd octave
-    float m2 = polynoise (r3 * (p * 2.0), .3);
-    m2 += polynoise (r2 * (p + vec2(0.2, 0.6)) * 2.0, .2);
+    float m2 = polynoise (r3 * (p * 2.0), (sin(u_Time * 0.3)));
+    m2 += polynoise (r2 * (p + vec2(0.2, 0.6)) * 2.0, (sin(u_Time * 0.3)));
     m1 += m2 * 0.5 * 0.25;
 	
     return m1;
@@ -95,7 +95,6 @@ float heightDisplacement(vec3 p)
 {
     vec3 c = (p);
     return (polyfbm(c.xz));
-    return sin(20.0 * c.x) * sin(20.0 * c.y) * sin(20.0 * c.z);
 }
 
 float opHeightDisplacement(vec3 p)
@@ -107,7 +106,7 @@ float opHeightDisplacement(vec3 p)
 // ------------------------------------------------
 
 vec2 map (vec3 pos) {
-    vec2 scene = vec2(opHeightDisplacement(pos), 50.0);
+    vec2 scene = vec2(opHeightDisplacement(pos), opHeightDisplacement(pos));
     return scene;
 }
 
@@ -143,10 +142,14 @@ vec3 estimateNormal (vec3 p) {
 }
 
 vec3 render (vec3 eye, vec3 dir) {
-    vec3 color = vec3 (0.7, 0.9, 1.0) + dir.y * 0.8;
+    vec3 color = vec3 (11.0 / 255.0, 0.0, 51.0 / 255.0) + dir.y * cos(u_Time * 0.03);
     vec2 result = raymarch(eye, dir, MIN_DIST, MAX_DIST);
     float x = result.x;
     float y = result.y;
+
+    vec3 darkPurple = vec3(55.0 / 255.0, 0.0, 49.0 / 255.0);
+    vec3 ruby = vec3(131.0 / 255.0, 34.0 / 255.0, 50.0 / 255.0);
+    vec3 copper = vec3(206.0 / 255.0, 137.0 / 255.0, 100.0 / 255.0);
 
     if (y > -0.5) {
         vec3 pos = eye + x * dir;
@@ -155,32 +158,43 @@ vec3 render (vec3 eye, vec3 dir) {
 
         // calculate material
         color = 0.45 + 0.35 * sin(vec3(0.05, 0.08, 0.10) * (y - 1.0));
-        if( y < 1.5 )
+        if (y < 1.0)
         {           
-            float f = mod(floor(5.0 * pos.z) + floor(5.0 * pos.x), 2.0);
-            color = 0.3 + 0.1 * f * vec3(1.0);
+            color = mix(darkPurple, ruby, 0.4);
+        }
+        if (y < 0.2)
+        {           
+            color = mix(darkPurple, ruby, 0.8);
+        }
+        if (y < 0.04)
+        {           
+            color = mix(copper, ruby, 0.8);
+        }
+        if (y < 0.008)
+        {           
+            color = mix(copper, ruby, 0.4);
         }
 
         //calculate lighting
         //float occ = calcAO( pos, nor );
-		vec3  lig = normalize( vec3(-0.4 * sin(u_Time * 0.03), 0.7, -0.6 * cos(u_Time * 0.03)));
-		float amb = clamp( 0.5+0.5*nor.y, 0.0, 1.0 );
-        float dif = clamp( dot( nor, lig ), 0.0, 1.0 );
-        float bac = clamp( dot( nor, normalize(vec3(-lig.x,0.0,-lig.z))), 0.0, 1.0 )*clamp( 1.0-pos.y,0.0,1.0);
-        float dom = smoothstep( -0.1, 0.1, ref.y );
-        float fre = pow( clamp(1.0+dot(nor, dir),0.0,1.0), 2.0 );
-		float spe = pow(clamp( dot( ref, lig ), 0.0, 1.0 ),16.0);
+		vec3  lig = normalize(vec3(-0.4 * sin(u_Time * 0.03), 0.7, -0.6 * cos(u_Time * 0.03)));
+		float amb = clamp(0.5 + 0.5 * nor.y, 0.0, 1.0);
+        float dif = clamp(dot(nor, lig), 0.0, 1.0);
+        float bac = clamp(dot(nor, normalize(vec3(-lig.x, 0.0, -lig.z))), 0.0, 1.0 ) * clamp(1.0-pos.y, 0.0, 1.0);
+        float dom = smoothstep(-0.1, 0.1, ref.y);
+        float fre = pow(clamp(1.0 + dot(nor, dir), 0.0, 1.0), 2.0);
+		float spe = pow(clamp(dot(ref, lig), 0.0, 1.0 ), 16.0);
 
         vec3 lin = vec3(0.0);
-        lin += 1.30*dif*vec3(1.00,0.80,0.55);
-		lin += 2.00*spe*vec3(1.00,0.90,0.70)*dif;
-        //lin += 0.40*amb*vec3(0.40,0.60,1.00)*occ;
-        //lin += 0.50*dom*vec3(0.40,0.60,1.00)*occ;
-        //lin += 0.50*bac*vec3(0.25,0.25,0.25)*occ;
-        //lin += 0.25*fre*vec3(1.00,1.00,1.00)*occ;
+        lin += 1.30 * dif * vec3(1.00, 0.80, 0.55);
+		lin += 2.00 * spe * vec3(1.00, 0.90, 0.70) * dif;
+        //lin += 0.40 * amb * vec3(0.40, 0.60, 1.00) * occ;
+        //lin += 0.50 * dom * vec3(0.40, 0.60, 1.00) * occ;
+        //lin += 0.50 * bac * vec3(0.25, 0.25, 0.25) * occ;
+        //lin += 0.25 * fre * vec3(1.00, 1.00, 1.00) * occ;
 		color = color * lin;
 
-    	color = mix( color, vec3(0.8,0.9,1.0), 1.0-exp( -0.0002*x*x*x ) );
+    	color = mix(color, vec3(0.8, 0.9, 1.0), 1.0 - exp(-0.0002 * x * x * x));
     }
 
     return vec3(clamp(color, 0.0, 1.0));
@@ -230,8 +244,9 @@ vec3 illumination (vec3 k_a, vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye) 
 }
 
 void main () {
-    vec3 dir = rayDirection(45.0, u_Dimensions, vec2(gl_FragCoord.x, gl_FragCoord.y));
+    vec3 dir = rayDirection(100.0, u_Dimensions, vec2(gl_FragCoord.x, gl_FragCoord.y));
     vec3 eye = vec3(0.0, 0.3, -0.5);
+    eye = u_Eye;
     //float dist = raymarch(eye, dir, MIN_DIST, MAX_DIST);
 
     //if (dist > MAX_DIST - EPSILON) {
