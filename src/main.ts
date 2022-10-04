4099030cd47d5f39d4d43c77bda0c1a7ab22b2d2
vivianliu <vivienliu1998@gmail.com>
2022-10-03 23:17:55 -0400

    Finished

--- src/main.ts
+++ src/main.ts
@@ -61,7 +61,9 @@ function main() {
   // Initial call to load scene
   loadScene();
 
-  const camera = new Camera(vec3.fromValues(0, 0, -10), vec3.fromValues(0, 0, 0));
+  const camera = new Camera(vec3.fromValues(0, -1.5, 10), vec3.fromValues(0, -1, 0));
+  // console.log(camera.controls.eye);
+  // console.log(camera.controls.center);
 
   const renderer = new OpenGLRenderer(canvas);
   renderer.setClearColor(164.0 / 255.0, 233.0 / 255.0, 1.0, 1);
