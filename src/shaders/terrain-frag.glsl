2815c9c3fd1d542b6dda2a3bb279acb61072fbf5
rcj9719 <rcj9719@gmail.com>
2022-10-04 23:17:57 -0400

    basic sdf geometry

--- src/shaders/terrain-frag.glsl
+++ src/shaders/terrain-frag.glsl
@@ -42,20 +42,70 @@ struct DirectionalLight
     vec3 color;
 };
 
-struct Geom {
-    float t;
-    int material_id;    
-};
-
 /*
  ******************************************************
  * Noise Functions
  ******************************************************
  */
 
+float hash2D( vec2 p ) { float h = dot(p,vec2(127.1,311.7)); return fract(sin(h)*43758.5453123); }
+
+float noise( in vec2 p ) {
+    vec2 i = floor( p );
+    vec2 f = fract( p );	
+	vec2 u = f*f*(3.0-2.0*f);
+    return -1.0+2.0*mix( mix( hash2D( i + vec2(0.0,0.0) ), 
+                     hash2D( i + vec2(1.0,0.0) ), u.x),
+                mix( hash2D( i + vec2(0.0,1.0) ), 
+                     hash2D( i + vec2(1.0,1.0) ), u.x), u.y);
+}
+
+
 float hash( float n )
 {
     return fract(sin(n)*43758.5453);
+}
+
+float random3D( vec3 x )
+{
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+
+    f = f*f*(3.0-2.0*f);
+
+    float n = p.x + p.y*57.0 + p.z * 50.0;
+
+    return mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
+               mix( hash(n+ 57.0), hash(n+ 58.0),f.x),f.y);
+}
+
+float interpNoise3D(vec3 p) {
+    int intX = int(floor(p.x));
+    float fractX = fract(p.x);
+    int intY = int(floor(p.y));
+    float fractY = fract(p.y);
+    int intZ = int(floor(p.z));
+    float fractZ = fract(p.z);
+
+    float v1 = random3D(vec3(intX, intY, intZ));
+    float v2 = random3D(vec3(intX, intY, intZ + 1));
+    float v3 = random3D(vec3(intX, intY + 1, intZ));
+    float v4 = random3D(vec3(intX, intY + 1, intZ + 1));
+
+    float v5 = random3D(vec3(intX + 1, intY, intZ));
+    float v6 = random3D(vec3(intX + 1, intY, intZ + 1));
+    float v7 = random3D(vec3(intX + 1, intY + 1, intZ));
+    float v8 = random3D(vec3(intX + 1, intY + 1, intZ + 1));
+
+    float i1 = mix(v1, v2, fractZ);
+    float i2 = mix(v3, v4, fractZ);
+    float i3 = mix(i1, i2, fractY);
+    
+    float i4 = mix(v5, v6, fractZ);
+    float i5 = mix(v7, v8, fractZ);
+    float i6 = mix(i4, i5, fractY);
+
+    return mix(i3, i6, fractX);
 }
 
 float random2D( vec2 x )
@@ -71,48 +121,82 @@ float random2D( vec2 x )
                mix( hash(n+ 57.0), hash(n+ 58.0),f.x),f.y);
 }
 
-
 float interpNoise2D(vec2 p) {
     int intX = int(floor(p.x));
     float fractX = fract(p.x);
     int intY = int(floor(p.y));
     float fractY = fract(p.y);
 
-    float v1 = random2D(vec2(intX, intY));
-    float v2 = random2D(vec2(intX + 1, intY));
-    float v3 = random2D(vec2(intX, intY + 1));
-    float v4 = random2D(vec2(intX + 1, intY + 1));
+    float v1 = hash2D(vec2(intX, intY));
+    float v2 = hash2D(vec2(intX + 1, intY));
+    float v3 = hash2D(vec2(intX, intY + 1));
+    float v4 = hash2D(vec2(intX + 1, intY + 1));
 
     float i1 = mix(v1, v2, fractX);
     float i2 = mix(v3, v4, fractX);
     return mix(i1, i2, fractY);
 }
 
-float fbm2D(vec2 p, float freq, float persistence, float amp) {
-    float total = 0.f;
-    //float persistence = 0.5f;
-    int octaves = 8;
-    //float freq = 2.f;
-    //float amp = 2.f;//0.5f;
-    for(int i = 1; i <= octaves; i++) {
-        total += interpNoise2D(p.xy * freq) * amp;
-        freq *= 2.f;
-        amp *= persistence;
-    }
-    return total;
-	// float v = 0.0;
-    // int octaves = 8;
-	// vec2 shift = vec2(100);
-	// // Rotate to reduce axial bias
-    // mat2 rot = mat2(cos(0.5), sin(0.5), -sin(0.5), cos(0.50));
-	// for (int i = 0; i < octaves; ++i) {
-	// 	v += amp * interpNoise2D(p);
-	// 	p = rot * p * 2.0 + shift;
-	// 	amp *= persistence;
-	// }
-	// return v;
+float surflet(vec2 P, vec2 gridPoint) {
+    // Compute falloff function by converting linear distance to a polynomial
+    float distX = abs(P.x - gridPoint.x);
+    float distY = abs(P.y - gridPoint.y);
+    float tX = 1.f - 6.f * pow(distX, 5.f) + 15.f * pow(distX, 4.f) - 10.f * pow(distX, 3.f);
+    float tY = 1.f - 6.f * pow(distY, 5.f) + 15.f * pow(distY, 4.f) - 10.f * pow(distY, 3.f);
+    // Get the random vector for the grid point
+    vec2 gradient = 2.f * random3D(vec3(gridPoint.xy, 1.0)) - vec2(1.f);
+    // Get the vector from the grid point to P
+    vec2 diff = P - gridPoint;
+    // Get the value of our height field by dotting grid->P with our gradient
+    float height = dot(diff, gradient);
+    // Scale our height field (i.e. reduce it) by our polynomial falloff function
+    return height * tX * tY;
 }
 
+float perlinNoise(vec2 uv) {
+	float surfletSum = 0.f;
+	// Iterate over the four integer corners surrounding uv
+	for(int dx = 0; dx <= 1; ++dx) {
+		for(int dy = 0; dy <= 1; ++dy) {
+			surfletSum += surflet(uv, floor(uv) + vec2(dx, dy));
+		}
+	}
+	return surfletSum;
+}
+
+float fbm2D(vec2 p, float freq, float persistence, float amp) {
+    // float total = 0.f;
+    // //float persistence = 0.5f;
+    // int octaves = 8;
+    // //float freq = 2.f;
+    // //float amp = 2.f;//0.5f;
+    // for(int i = 1; i <= octaves; i++) {
+    //     total += interpNoise2D(p.xy * freq) * amp;
+    //     freq *= 2.f;
+    //     amp *= persistence;
+    // }
+    // return total;
+	float v = 0.0;
+    int octaves = 8;
+	vec2 shift = vec2(100);
+	// Rotate to reduce axial bias
+    mat2 rot = mat2(cos(0.5), sin(0.5), -sin(0.5), cos(0.50));
+	for (int i = 0; i < octaves; ++i) {
+		v += amp * interpNoise2D(p);
+		p = rot * p * 2.0 + shift;
+		amp *= persistence;
+	}
+	return v;
+}
+
+float mountainHeightFunc(vec3 p) {
+    //vec2 randomOffset = vec2(hash(p.x/12.f), hash(p.z/12.f)) * 0.2f;
+    // vec2 randomOffset = vec2(0.3f, 0.3f);
+    float fbmMountains = fbm2D((p.xz) / 12.f, 0.5, 0.2, 18.f); //fbm2D((p.xz) * 0.007f, 0.6, 0.2, 400.f);
+    float terrainNoise = -1.f * fbm2D(p.xz, 0.5, 0.5, 0.8f);
+    float yOffset = -2.f;
+    return  fbmMountains + terrainNoise + yOffset;
+}   
 
 /*
  ******************************************************
@@ -129,18 +213,6 @@ float planeSDF(vec3 queryPos, float height)
     return queryPos.y - height;
 }
 
-float boxSDF( vec3 p, vec3 b )
-{
-  vec3 q = abs(p) - b;
-  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
-}
-
-float roundBoxSDF( vec3 p, vec3 b, float r )
-{
-  vec3 q = abs(p) - b;
-  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - r;
-}
-
 float capsuleSDF( vec3 queryPos, vec3 a, vec3 b, float r )
 {
   vec3 pa = queryPos - a, ba = b - a;
@@ -148,22 +220,10 @@ float capsuleSDF( vec3 queryPos, vec3 a, vec3 b, float r )
   return length( pa - ba*h ) - r;
 }
 
-float torusSDF( vec3 p, vec2 t )
-{
-  vec2 q = vec2(length(p.xz)-t.x,p.y);
-  return length(q)-t.y;
-}
-
 float roundedCylinderSDF( vec3 queryPos, float ra, float rb, float h )
 {
   vec2 d = vec2( length(queryPos.xz)-2.0*ra+rb, abs(queryPos.y) - h );
   return min(max(d.x,d.y),0.0) + length(max(d,0.0)) - rb;
-}
-
-float cappedCylinderSDF( vec3 queryPos, float h, float r )
-{
-  vec2 d = abs(vec2(length(queryPos.xz), queryPos.y)) - vec2(h,r);
-  return min(max(d.x,d.y),0.0) + length(max(d,0.0));
 }
 
 float smoothUnion( float d1, float d2, float k ) {
@@ -183,138 +243,34 @@ float smoothIntersection( float d1, float d2, float k )
     return mix( d2, d1, h ) + k*h*(1.0-h);
 }
 
-// vec3 opTx( in vec3 p, in transform t, in sdf3d primitive )
-// {
-//     return primitive( invert(t)*p );
-// }
+float sceneSDF(vec3 p, inout float mtHeight) {
 
-// float opScale( in vec3 p, in float s, in sdf3d primitive )
-// {
-//     return primitive(p/s)*s;
-// }
+    //float dS = sphereSDF(p, vec3(0,1,0), 1.f);
+    mtHeight = mountainHeightFunc(p);
+    //float dMountain = planeSDF(p, mtHeight);
 
-float opSmoothUnion( float d1, float d2, float k ) {
-    float h = clamp( 0.5 + 0.5*(d2-d1)/k, 0.0, 1.0 );
-    return mix( d2, d1, h ) - k*h*(1.0-h); }
-
-float opSmoothSubtraction( float d1, float d2, float k ) {
-    float h = clamp( 0.5 - 0.5*(d2+d1)/k, 0.0, 1.0 );
-    return mix( d2, -d1, h ) + k*h*(1.0-h); }
-
-float opSmoothIntersection( float d1, float d2, float k ) {
-    float h = clamp( 0.5 - 0.5*(d2-d1)/k, 0.0, 1.0 );
-    return mix( d2, d1, h ) + k*h*(1.0-h); }
-
-// vec3 rotateY(vec3 p, float amt) {
-//     return vec3(cos(amt) * p.x + sin(amt) * p.z, p.y, -sin(amt) * p.x + cos(amt) * p.z);
-// }
-
-// vec3 rotateX(vec3 p, float amt) {
-//     return vec3(p.x, cos(amt) * p.y - sin(amt) * p.z, sin(p.y) + cos(p.z));
-// }
-
-// vec3 rotateZ(vec3 p, float amt) {
-//     return vec3(cos(amt) * p.x - sin(amt) * p.y, cos(amt) * p.x + sin(amt) * p.y, p.z);
-// }
-
-float mountainHeightFunc(vec3 p) {
-    //vec2 randomOffset = vec2(hash(p.x/12.f), hash(p.z/12.f)) * 0.2f;
-    // vec2 randomOffset = vec2(0.3f, 0.3f);
-    float fbmMountains = 0.f; //fbm2D((p.xz) / 12.f, 0.5, 0.2, 18.f); //fbm2D((p.xz) * 0.007f, 0.6, 0.2, 400.f);
-    float terrainNoise = 1.f * fbm2D(p.xz, 0.8, 0.5, 0.2f);
-    float yOffset = -0.7f;//-2.f;
-    return  fbmMountains + terrainNoise + yOffset;
-}  
-
-mat2 rot(float a) {
-    float s = sin(a);
-    float c = cos(a);
-    return mat2(c, -s, s, c);
-}
-
-float sceneSDF(vec3 p) {
-
-    // move down
-    p += vec3(0.0, 3.0, 0.0);
-
-    // coffee mug
     float dMugOuter = roundedCylinderSDF( p, 0.6f, 0.2f, 1.f);
     float dMugInner = roundedCylinderSDF( p - vec3(0.f, 0.4f, 0.f), 0.5f, 0.2f, 1.f);
-    // vec3 rotatedP = rotateZ(p, 60.f * 3.14f/ 180.f);
-    vec3 pRotMugHandle = p + vec3(-1.5f, 0.0, 0.0);
-    pRotMugHandle.yz *= rot(90.f * 3.14f/ 180.f);
-    float dMugHandle = torusSDF(pRotMugHandle, vec2(0.7f, 0.07));
+    float dMug = max(dMugOuter, -dMugInner);
 
-    float dMugTemp = min(dMugOuter, dMugHandle);//smoothUnion(dMugOuter, dMugHandle, 0.2f);
-    float dMug = max(dMugTemp, -dMugInner);
+    //float d = min(dMug, dMountain);
+    float d = dMug;
+    return d;
     
-    float dCoffee = cappedCylinderSDF(p - vec3(0.f, 0.4f, 0.f), 1.f, 0.01f);
-    float dCoffeeMug = min(dCoffee, dMug);
+    // vec3 capsuleA = vec3(-1.f, 1.f, 2.f);
+    // vec3 capsuleB = vec3(1.f, 1.f, 1.f);
+    // float dRiverBed = capsuleSDF(p, capsuleA, capsuleB, 10.f);
+    //float dRiver = max(dMountain, -dRiverBed);
+    //float dWater = planeSDF(p, 2.f);
+    // dRiver = max(dWater, dRiver);
+    // float d = dRiver;
 
-    Geom gCoffeeMug;
-    gCoffeeMug.t = dCoffeeMug;
-    gCoffeeMug.material_id = 0;
-
-    // pen
-    vec3 pRotPen = vec3(p + vec3(2.5f, 0.3, 0.0));
-    float a = 90.f * 3.14f/ 180.f;
-    // mat3 rotateX = mat3(vec3(1.f, 0.0, 0.0),
-    //                     vec3(0, cos(a), sin(a) ),
-    //                     vec3(0, -sin(a), cos(a)));
-
-    // mat3 rotateY = mat3(vec3(cos(a), 0.0, -sin(a)),
-    //                     vec3(0, 1.f, 0.f ),
-    //                     vec3(sin(a), 0.f, cos(a)));   
-
-    // pRotPen = rotateY * rotateX * pRotPen;
-    pRotPen.xz = rot(45.f * 3.14f/ 180.f) * pRotPen.xz;    // rotation about Y-axis
-    pRotPen.yz = rot(90.f * 3.14f/ 180.f) * pRotPen.yz;   // rotation about X-axis
-    float dPenBase = roundedCylinderSDF( pRotPen, 0.02f, 0.05f, 1.f);
-    float dPenCap = roundedCylinderSDF( pRotPen - vec3(0.0, 0.7f, 0.0), 0.03f, 0.05f, 0.4f);
-    float dPenCapHook = boxSDF( pRotPen - vec3(0.07, 0.7f, 0.0), vec3(0.02, 0.4f, 0.02));
-    float dPen = min(min(dPenCap, dPenBase), dPenCapHook);
-
-    Geom gPen;
-    gPen.t = dPen;
-    gPen.material_id = 0;
-
-    // closed book
-    vec3 pCBook = p + vec3(2.5f, 0.80, 0.0);
-
-    float dCBookCover = roundBoxSDF(pCBook, vec3(0.8f, 0.25f, 1.1f), 0.2);
-    float dCBookPages = boxSDF(pCBook, vec3(0.9f, 0.28f, 1.2f));
-    float dCBookPagesCut = boxSDF(pCBook + vec3(-0.2f, 0.f, 0.f), vec3(1.1f, 0.28f, 1.8f));
-    float dCBook = min(max(dCBookCover, -dCBookPagesCut), dCBookPages);
-
-    Geom gCBook;
-    gCBook.t = dCBook;
-    gCBook.material_id = 0;
-
-    // textured plane
-    float dP = planeSDF(p + vec3(0.0, 1.75f, 0.0), fbm2D(p.xz, 0.8, 0.5, 0.2f));
-    
-    float dMountain = 0.f;
-    if(p.x > 125.f || p.z > 125.f || p.x < -125.f || p.z < -125.f){
-        dMountain = planeSDF(p + vec3(0.0, 1.75f, 0.0), fbm2D((p.xz) / 12.f, 0.5, 0.2, 18.f));
-        dP = dMountain;
-    }
-
-    Geom gDeskPlane;
-    gDeskPlane.t = dP;
-    gDeskPlane.material_id = 0;
-    
-    Geom allGeoms[4] = Geom[4](gCoffeeMug, gPen, gCBook, gDeskPlane);
-    float min = MAX_DIST;
-    for(int i = 0; i < 4; i++) {
-        if(min > allGeoms[i].t) {
-            min = allGeoms[i].t;
-        }
-    }
-    return min;
-    // float d = min(dCBook, min(dPen, dMug));//min(dCBook, dCBookPages);
-    // float d = min(dPen, dMug);//min(dP, min(dPen, min(dCoffeeMug, dCBook))); //min(dPen, min(dMug, dCBook));
+    // vec4 sphere = vec4(0,1,0,1);    // center.xyz,radius
+    // float dS = length(p - sphere.xyz) - sphere.w; // dist from sphere = dist from center - radius
+    // float dP = p.y; // dist from axis aligned plane
+    // float d = min(dS,dP);
+    // // float d = dS;
     // return d;
-
 }
 
 /*
@@ -322,6 +278,13 @@ float sceneSDF(vec3 p) {
  * Ray Marching
  ******************************************************
  */
+
+ 
+// vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
+//     vec2 xy = fragCoord - size / 2.0;
+//     float z = size.y / tan(radians(fieldOfView) / 2.0);
+//     return normalize(vec3(xy, -z));
+// }
 
 Ray getRay(vec2 uv) {
     Ray ray;
@@ -335,21 +298,32 @@ Ray getRay(vec2 uv) {
     vec3 p = u_Ref + uv.x * H + uv.y * V;
     vec3 dir = normalize(p - u_Eye);
 
+    // vec3 dir = rayDirection(45.0f, u_Dimensions.xy, gl_FragCoord.xy);
+    // vec3 eye = vec3(0.0, 0.0, 5.0);
+
     ray.origin = u_Eye;
     ray.direction = dir;
     return ray;
 }
 
 
-Intersection getRaymarchedIntersection(vec2 uv)
+Intersection getRaymarchedIntersection(vec2 uv, out float mtHeight)
 {
     Ray ray = getRay(uv);
     Intersection intersection;
+    
+    // if(ray.origin.x > 10.f || ray.origin.x < -10.f ||
+    //     ray.origin.z > 10.f || ray.origin.z < -10.f ){
+    //         intersection.position = ray.origin;
+    //         intersection.distance = 0.f;
+    //         intersection.normal = vec3(0.0, 0.0, 1.0);
+    //         return intersection;
+    // }
 
     vec3 queryPoint = ray.origin;
     for (int i=0; i < MAX_STEPS; ++i)
     {
-        float distanceToSurface = sceneSDF(queryPoint);
+        float distanceToSurface = sceneSDF(queryPoint, mtHeight);
         
         if (distanceToSurface < EPSILON)
         {
@@ -385,16 +359,18 @@ Intersection getRaymarchedIntersection(vec2 uv)
  ******************************************************
  */
 vec3 estimateNormal(vec3 p) {
+    float mtHeightTemp;
     return normalize(vec3(
-        sceneSDF(vec3(p.x + EPSILON, p.y, p.z)) - sceneSDF(vec3(p.x - EPSILON, p.y, p.z)),
-        sceneSDF(vec3(p.x, p.y + EPSILON, p.z)) - sceneSDF(vec3(p.x, p.y - EPSILON, p.z)),
-        sceneSDF(vec3(p.x, p.y, p.z  + EPSILON)) - sceneSDF(vec3(p.x, p.y, p.z - EPSILON))
+        sceneSDF(vec3(p.x + EPSILON, p.y, p.z), mtHeightTemp) - sceneSDF(vec3(p.x - EPSILON, p.y, p.z), mtHeightTemp),
+        sceneSDF(vec3(p.x, p.y + EPSILON, p.z), mtHeightTemp) - sceneSDF(vec3(p.x, p.y - EPSILON, p.z), mtHeightTemp),
+        sceneSDF(vec3(p.x, p.y, p.z  + EPSILON), mtHeightTemp) - sceneSDF(vec3(p.x, p.y, p.z - EPSILON), mtHeightTemp)
     ));
 }
 
 vec3 getSceneColor(vec2 uv)
 {
-    Intersection intersection = getRaymarchedIntersection(uv);
+    float mtHeight;
+    Intersection intersection = getRaymarchedIntersection(uv, mtHeight);
     
     DirectionalLight lights[3];
     vec3 backgroundColor = vec3(0.);
@@ -413,7 +389,17 @@ vec3 getSceneColor(vec2 uv)
                                  SUN_AMBIENT_LIGHT);
     backgroundColor = SUN_KEY_LIGHT;
     
-    vec3 albedo = vec3(0.5, 0.9, 0.7);
+    vec3 waterColor = vec3(0.0, 0.0, 1.0);
+    vec3 iceColor = vec3(1.0, 1.0, 1.0);
+    vec3 terrainColor = vec3(0.5, 0.9, 0.7);
+
+    vec3 albedo = terrainColor;
+    // if(mtHeight > 12.f) {
+    //     albedo = iceColor;
+    // }
+    // else if(mtHeight < 2.f) {
+    //     albedo = waterColor;
+    // }
 
     vec3 n = estimateNormal(intersection.position);
         
